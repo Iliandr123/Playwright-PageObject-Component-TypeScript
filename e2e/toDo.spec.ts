d6c977b5b0ca242cc0afd472cb6f7702c548d3b6
Iliandr123 <isapronov@tourmalinecore.com>
2023-08-28 00:40:45 +0500

    refactor: turn page object into playwright fixture

--- e2e/toDo.spec.ts
+++ e2e/toDo.spec.ts
@@ -1,14 +1,11 @@
-import { test, expect } from '@playwright/test';
-import { TodoPage } from '../pages/toDo-page';
+import { searchTest as test } from './tests';
 
-test.beforeEach(async ({ page }) => {
-    const todoPage = new TodoPage(page);
+test.beforeEach(async ({ todoPage }) => {
     await todoPage.visit("https://demo.playwright.dev/todomvc/#/");
 })
 
 test.describe("Test todo", () => {
-    test('create todo', async ({ page }) => {
-        const todoPage = new TodoPage(page);
+    test('create todo', async ({ todoPage }) => {
         await todoPage.titleHaveText("todo");
         await todoPage.titleToBeVisible();
     })
